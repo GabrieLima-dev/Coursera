5234f0f064de135af5cb95625b8417f0e81d38e4
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-06-24 21:05:33 -0300

    Correção de Valores e alteração de nome.

---
+++ perimetro_area.py
@@ -0,0 +1,5 @@
+quadrado = int(input("Digite o valor correspondente ao lado de um quadrado: "))
+perimetro = str(quadrado**4)
+area = str(quadrado**2)
+
+print("Perímetro: " + perimetro + " Área: " + area)
