9be919d87e0cc022c3b19adc210a8f48e8202267
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-06-24 21:15:06 -0300

    Removido

--- exercicio_1.py
+++
@@ -1,6 +0,0 @@
-# Exercício 1 1
-quadrado = int(input("Digite o valor correspondente ao lado de um quadrado: "))
-perimetro = str(4 * quadrado)
-area = str(2 * quadrado)
-
-print("Perímetro: " + perimetro + " Área: " + area)
