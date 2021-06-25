a154ef226eb63efb0a86bf873f9e5e1e5bbd6d75
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-06-24 21:25:47 -0300

    Incremento Hífen

--- perimetro_area.py
+++ perimetro_area.py
@@ -2,4 +2,4 @@ quadrado = int(input("Digite o valor correspondente ao lado de um quadrado: "))
 perimetro = str(4*quadrado)
 area = str(quadrado**2)
 
-print("Perímetro: " + perimetro + " Área: " + area)
+print("perímetro: " + perimetro + " - área: " + area)
