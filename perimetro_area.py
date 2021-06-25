fc3068a547ce85af61cbce45cd1d68cc15706845
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-06-24 21:21:59 -0300

    Correção 2

--- perimetro_area.py
+++ perimetro_area.py
@@ -1,5 +1,5 @@
 quadrado = int(input("Digite o valor correspondente ao lado de um quadrado: "))
-perimetro = str(quadrado**4)
+perimetro = str(4*quadrado)
 area = str(quadrado**2)
 
 print("Perímetro: " + perimetro + " Área: " + area)
