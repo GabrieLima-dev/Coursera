6694bac97d80fdf12290b9f6e7be0a93da41d45f
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-06-24 21:54:11 -0300

    Correção

--- segundos.py
+++ segundos.py
@@ -1,14 +1,16 @@
 entrada_segundos = int(
     input("Por favor, entre com o número de segundos que deseja converter: "))
 
-n_minutos = int(entrada_segundos/60)
-n_segundos = int(n_minutos % 60)
-n_horas = int(n_minutos/60)
-n_dias = int(n_horas/24)
-minutos = str(n_minutos)
-segundos = str(n_segundos)
-horas = str(n_horas)
-dias = str(n_dias)
+horas = entrada_segundos // 3600
+dias = horas // 86400
+segundos_restantes = entrada_segundos % 3600
+minutos = segundos_restantes // 60
+segundos_restantes_final = segundos_restantes % 60
 
-print(dias + " dias " + horas + " horas, " + minutos +
-      " minutos e " + segundos + " segundos. ")
+horas_str = str(horas)
+dias_str = str(dias)
+minutos_str = str(minutos)
+segundos_str = str(segundos_restantes_final)
+
+print(dias_str + " dias " + horas_str + " horas, " + minutos_str +
+      " minutos e " + segundos_str + " segundos. ")
