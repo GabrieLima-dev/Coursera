d98f4d8e7330c2dba919750901a1b7b2aaeb8018
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-06-25 10:41:19 -0300

    Verificando ordenação

---
+++ crescente.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um valor inteiro: "))
+b = int(input("Digite um valor inteiro: "))
+c = int(input("Digite um valor inteiro: "))
+
+if a <= b and b <= c:
+    print("crescente")
+else:
+    print("não está em ordem crescente")
