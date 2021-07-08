fd91613c9d5de8abca3b8b4ec5050497b3cda65a
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-07-07 23:44:07 -0300

    Função Máximo com 3 int

---
+++ maximo_3.py
@@ -0,0 +1,9 @@
+def maximo(x, y, z):
+    return max(x, y, z)
+
+
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+c = int(input("Digite outro número: "))
+
+print(maximo(a, b, c))
