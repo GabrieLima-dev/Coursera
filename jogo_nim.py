08fcd2bb7001ff6f10a9ceeb11ffe71ef13dc98d
Gabriel Lima <gabrieldesouzalima@gmail.com>
2021-07-13 18:43:50 -0300

    Jogo completo (alteração def main)

--- jogo_nim.py
+++ jogo_nim.py
@@ -79,24 +79,22 @@ def campeonato():
     print("Placar final: Você  {} x {}  Computador".format(usuario, computador))
 
 
-while tipo_jogo == 0:
+def main(x):
+    while True:
+        if x == 1:
+            print("Você escolheu uma partida Isolada!")
+            partida()
+            break
+        if x == 2:
+            print("Voce escolheu um campeonato!")
+            campeonato()
+            break
+        if x != 1 or x != 2:
+            print("Essa opção não é válida!")
+            x = int(input(
+                "Bem-vindo ao jogo do NIM! Escolha: \n \n 1 - para jogar uma partida isolada \n 2 - para jogar um campeonato "))
 
-    print("Bem-vindo ao jogo do NIM! Escolha:")
-    print(" ")
-    print("1 - Para jogar uma partida isolada")
-    print("2 - Para jogar um campeonato")
 
-    tipo_jogo = int(input("Sua opção: "))
-    print(" ")
-
-    if tipo_jogo == 1:
-        print("Voce escolheu partida isolada!")
-        partida()
-        break
-    if tipo_jogo == 2:
-        print("Voce escolheu um campeonato!")
-        campeonato()
-        break
-    else:
-        print("Opção inválida!")
-        tipo_jogo = 0
+x = int(input("Bem-vindo ao jogo do NIM! Escolha: \n \n 1 - para jogar uma partida isolada \n 2 - para jogar um campeonato "))
+print("")
+main(x)
